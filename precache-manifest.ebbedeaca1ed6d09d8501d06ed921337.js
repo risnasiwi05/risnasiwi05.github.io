189b7385f82a532d5137a07b8ee8da10037b288e
Risnasiwi <risnasiwi1412@gmail.com>
2020-03-06 16:31:32 +0700

    deploy

---
+++ precache-manifest.ebbedeaca1ed6d09d8501d06ed921337.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d522d02f771ffea8bada1c7d5c04a3bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7af815e8b9884e2bbfba",
+    "url": "/static/css/main.ff6fe7c8.chunk.css"
+  },
+  {
+    "revision": "f35f669e4c955fbf7c39",
+    "url": "/static/js/2.f469382b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.f469382b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7af815e8b9884e2bbfba",
+    "url": "/static/js/main.469be2f5.chunk.js"
+  },
+  {
+    "revision": "2fc8e7369f6406e5984d",
+    "url": "/static/js/runtime-main.447fd809.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
